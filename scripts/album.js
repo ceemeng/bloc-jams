8dacbb171f0b8adf9f9b1c3b8351be47f2d217f8
Cheang Meng <cmengid@gmail.com>
2017-04-27 15:41:51 -0700

    assignment-11-dom-album completed

--- scripts/album.js
+++ scripts/album.js
@@ -25,8 +25,24 @@
          { title: 'Hello, Operator?', duration: '1:01' },
          { title: 'Ring, ring, ring', duration: '5:01' },
          { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Can you hear me now', duration: '3:14' },
          { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ // My Album
+ var albumMeng = {
+     title: 'Bloc Journey',
+     artist: 'Cheang Meng',
+     label: 'CM Records',
+     year: '2017',
+     albumArtUrl: 'assets/images/album_covers/16.png',
+     songs: [
+         { title: 'Front-End Freestyle', duration: '2:01' },
+         { title: 'The Scope', duration: '4:01' },
+         { title: 'Roadtrip to Javascript', duration: '2:21'},
+         { title: 'Git Real', duration: '4:14' },
+         { title: 'CSS, Started From the Top', duration: '3:15'}
      ]
  };
 
@@ -42,24 +58,24 @@
      return template;
  };
 
- var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ // Select elements that we want to populate with text dynamically
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-     // #2
+ var setCurrentAlbum = function(album) {
+     // Assign values to each part of the album (text, images)
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
 
-     // #3
+     // Clear contents of album song list container
      albumSongList.innerHTML = '';
 
-     // #4
+     // Build list of songs from album Javascript object
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
@@ -67,4 +83,14 @@
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var albums = [albumPicasso, albumMarconi, albumMeng];
+     var index = 1;
+     albumImage.addEventListener("click", function(event) {
+         setCurrentAlbum(albums[index]);
+         index++;
+         if (index == albums.length) {
+             index = 0;
+         }
+     });
  };
